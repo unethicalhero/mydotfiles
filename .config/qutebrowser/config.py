ead4dcd2ae927cb3b93635ff1f39054757181afd
unethicalhero <delusionaldreamer@tutanota.com>
2025-05-28 19:48:49 +0300

    Added qutebrowser simple config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,33 @@
+import os
+from urllib.request import urlopen
+import catppuccin
+
+config.load_autoconfig()
+
+c.url.searchengines = {
+    'DEFAULT': 'https://search.brave.com/search?q={}',
+    '!ddg': 'https://duckduckgo.com/?q={}',
+    '!aw': 'https://wiki.archlinux.org/?search={}',
+    '!apkg': 'https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=',
+    '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
+    '!yt': 'https://www.youtube.com/results?search_query={}',
+    }
+
+c.completion.open_categories = ['searchengines', 'quickmarks', 'bookmarks', 'history', 'filesystem']
+
+# c.auto_save.session = True
+
+if not os.path.exists(config.configdir / "theme.py"):
+    theme = "https://raw.githubusercontent.com/catppuccin/qutebrowser/main/setup.py"
+    with urlopen(theme) as themehtml:
+        with open(config.configdir / "theme.py", "a") as file:
+            file.writelines(themehtml.read().decode("utf-8"))
+
+if os.path.exists(config.configdir / "theme.py"):
+    import theme
+    theme.setup(c, 'mocha', True)
+
+# c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.darkmode.algorithm = 'lightness-cielab'
+c.colors.webpage.darkmode.policy.images = 'never'
+config.set('colors.webpage.darkmode.enabled', False, 'file://*')
